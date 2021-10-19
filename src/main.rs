6aac2bb37b23f7206dee41eaf1572a68bd08b69f
Maximilian von Gaisberg <git@vongaisberg.de>
2021-10-19 20:58:02 +0200

    Better logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+#![feature(cell_update)]
+
 use futures::prelude::*;
 use mongodb::bson::{doc, to_document, Bson, Document};
 use mongodb::{options::ClientOptions, Client};
 use serde_json::{Result, Value};
+use std::cell::Cell;
 use std::collections::HashMap;
+use std::time::{Duration, Instant};
 use twitter_stream::builder::FilterLevel;
 use twitter_stream::{Token, TwitterStream};
 use url::Url;
@@ -19,64 +23,42 @@ async fn main() {
         env::var("TOKEN").unwrap(),
         env::var("TOKEN_SECRET").unwrap(),
     );
-    let mut url_map = HashMap::<String, u64>::new();
+    //let mut url_map = HashMap::<String, u64>::new();
 
     let client = connect().await;
     let db = client.database("twitter");
     let tweets = db.collection::<Document>("tweets");
     //twitter_stream::Builder::new(token).
-
+    let now = Cell::new(Instant::now());
+    let count = Cell::new(0u64);
+    let count_total = Cell::new(0u64);
     TwitterStream::sample(&token)
         .try_flatten_stream()
         .for_each(|json| async {
-            //println!("New");
-            let v: Value = serde_json::from_str(&json.unwrap()).unwrap();
-            println!("{}", v["text"]);
-            // println!("{}\n{}\n", v["text"], v["entities"]["urls"]);try_
-            /* if let Some(urls) = v["entities"]["urls"].as_array() {
-                            if urls.len() > 0 {
-                                println!("{}", urls[0]["expanded_url"].as_str().unwrap());
+            count.update(|v| v + 1);
+            count_total.update(|v| v + 1);
+            if now.get().elapsed().as_secs() > 10 {
+                println!(
+                    "Tweets pro Sekunde: {}, Gesamtanzahl: {}",
+                    count.get() as f64 / now.get().elapsed().as_millis() as f64 * 100f64,
+                    count_total.get()
+                );
+                now.set(Instant::now());
+                count.set(0);
+            }
+            match json {
+                Ok(json) => {
+                    let v: Value = serde_json::from_str(&json).unwrap();
+                    tweets
+                        .insert_one(to_document(&v).unwrap(), None)
+                        .await
+                        .unwrap();
+                }
 
-                            let urls2: Vec<Document> = urls
-                                .iter()
-                                .map(|u| {
-                                    println!("{}", u);
-                                    /*let bytes = u.to_string().as_bytes().to_owned();
-                                    let slice = bytes.as_slice();
-                                    println!("Slice: {:?}", slice);*/
-                                    //Document::from_reader(slice).unwrap()
-                                    to_document(u).unwrap()
-                                })
-                                .collect();
-                                println!("Vec<Document>: {:?}", urls2);
-            */
-            tweets
-                .insert_one(to_document(&v).unwrap(), None)
-                .await
-                .unwrap();
-
-            /*
-            for url_str in urls {
-                if let Ok(url) = Url::parse(url_str["expanded_url"].as_str().unwrap()) {
-                    let tld = url.host_str().unwrap();
-                    *url_map.entry(tld.to_string()).or_insert(0) += 1;
+                Err(err) => {
+                    println!("Could not unwrap json: {}", err);
                 }
             }
-
-            let mut count_vec: Vec<(&String, &u64)> = url_map.iter().collect();
-            count_vec.sort_by(|a, b| b.1.cmp(a.1));
-            let mut i = 0;
-            for entry in count_vec {
-                i = i + 1;
-                if i > 20 {
-                    break;
-                }
-                println!("{}: {}", entry.0, entry.1);
-            }
-            println!("\n\n");
-            */
-
-            //  future::ok(())
         })
         .await;
 }
